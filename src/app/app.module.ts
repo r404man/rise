4f885ad3ae8e4a7713f31f1d0f323881c6718598
r404man <tyrtychko96rus@mail.ru>
2021-04-27 15:04:02 +0100

    2704/r404man

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/parts/nav-bar/nav-bar.component';
+import { SocialBarComponent } from './components/parts/social-bar/social-bar.component';
+import { MainComponent } from './components/pages/main/main.component';
+import { HeaderComponent } from './components/parts/header/header.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SocialBarComponent,
+    MainComponent,
+    HeaderComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
