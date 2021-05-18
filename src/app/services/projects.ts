436cba8a2fef7b1e72d6fc265c9c8e178704cef4
r404man <tyrtychko96rus@mail.ru>
2021-05-18 22:59:06 +0100

    1805/r404man/Media update

--- src/app/services/projects.ts
+++ src/app/services/projects.ts
@@ -1,35 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Observable, observable, of } from 'rxjs'
 
-// import { Project } from '../interfaces/project';
-
-// import { ProjectW as ProjectInterface } from '../interfaces/projectW';
-
 @Injectable({
   providedIn: 'root'
 })
 
 export class ProjectService {
-  //   project: ProjectInterface;
-  isAuth: boolean = true;
-
+  isAuth: boolean = false;
+  
   constructor() { }
-
-  //   getProjects(): Observable<Project[]> {
-  //     // return of(projects);
-  //   }
-
-  //   getProject(id: number): ProjectInterface {
-  //     ProjectData.map((val: ProjectInterface) => {
-  //       if (val.id === id) this.project = val
-  //     })
-  //     return this.project;
-  //   }
-
+  
+  
   auth(username: string, password: string): void {
     if (username === 'root' && password === 'root') {
       this.isAuth = true;
     }
   }
-
+  
 }
+
+
+//   project: ProjectInterface;
+
+//   getProjects(): Observable<Project[]> {
+//     // return of(projects);
+//   }
+
+//   getProject(id: number): ProjectInterface {
+//     ProjectData.map((val: ProjectInterface) => {
+//       if (val.id === id) this.project = val
+//     })
+//     return this.project;
+//   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCo2zatU19oSGTBKk80lXZtGX8Ef__rgio",
+    authDomain: "leafy-racer-310911.firebaseapp.com",
+    databaseURL: "https://leafy-racer-310911-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "leafy-racer-310911",
+    storageBucket: "leafy-racer-310911.appspot.com",
+    messagingSenderId: "375281731666",
+    appId: "1:375281731666:web:c120a473075d8be2b5f78b",
+    measurementId: "G-QQK9TJEQLB"
+
+  },
 };
